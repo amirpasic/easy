01d5920813bc94d51877a67e120ce7e5ca676617
amirpasic <146943460+amirpasic@users.noreply.github.com>
2023-10-04 12:49:29 +0200

    Create motor.js

---
+++ motor.js
@@ -0,0 +1,19 @@
+const pageStatus = 1;
+
+if (pageStatus === 1) {
+  const overlay = document.querySelector('div'); // Finde das Overlay-Element
+  if (overlay) {
+    overlay.remove(); // Entferne das Overlay
+  }
+  document.body.style.overflow = 'auto'; // Erlaube das Scrollen auf der Seite
+} else {
+  const overlay = document.createElement('div');
+  overlay.style.position = 'fixed';
+  overlay.style.top = '0';
+  overlay.style.left = '0';
+  overlay.style.width = '100%';
+  overlay.style.height = '100%';
+  overlay.style.background = 'gray';
+  document.body.appendChild(overlay);
+  document.body.style.overflow = 'hidden';
+}
